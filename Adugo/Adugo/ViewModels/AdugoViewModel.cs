02d98eb4f213edebf16a24b9f0a87b01b9d9242b
PiotrSzczap <piotrszczap93@gmail.com>
2015-03-30 19:05:59 +0200

    Prolog.NET added

---
+++ Adugo/Adugo/ViewModels/AdugoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Adugo.ViewModels
+{
+    class AdugoViewModel : INotifyPropertyChanged
+    {
+
+
+        public AdugoViewModel()
+        {
+            color = "red";
+
+        }
+
+        private string _color;
+
+        public string color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (value != _color)
+                {
+                    _color = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        #endregion
+    }
+}
---
+++ Adugo/Adugo/Views/AdugoMainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Adugo.ViewModels;
+
+namespace Adugo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private AdugoViewModel adugoViewModel;
+        public MainWindow()
+        {
+            adugoViewModel = new AdugoViewModel();
+            DataContext = adugoViewModel;
+            InitializeComponent();
+        }
+
+        
+    }
+}
